0e8a04dca519d5bad65088a3a228e2eaf571c1e8
alekseygenerozov <aleksey.generozov@gmail.com>
2019-06-11 19:10:27 -0600

    Adding space.

--- grid_plot.py
+++ grid_plot.py
@@ -7,5 +7,5 @@ import sys
 Mbh=10.0**float(sys.argv[1])*cgs.M_sun
 Ndet=eros.Ngrid(Mbh, 1.2, spin=float(sys.argv[2]))
 f=open('./Ndet_{0}_{1}'.format(sys.argv[1], sys.argv[2]), 'w')
-f.write('{0:e}'.format(Ndet))
+f.write('{0:e}\n'.format(Ndet))
 f.close()
